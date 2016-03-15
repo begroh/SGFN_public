336e8fc043f48b9ed727ba71fcf9d75094c7df36
Nick Essenburg <nessenbu@umich.edu>
2016-03-15 15:46:43 -0400

    Base class for guns

---
+++ Assets/Scripts/Guns/GunBase.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class GunBase : MonoBehaviour {
+
+    public Bullet bullet;
+    public float spread;
+    public float numBullets;
+    public float reloadTime;
+    public float bulletSpeed;
+
+    private float lastFireTime;
+    private Vector2 launchVec;
+
+    void Start()
+    {
+        lastFireTime = Time.time;
+        launchVec = new Vector2(bulletSpeed, 0);
+    }
+
+    public void Fire()
+    {
+        if (lastFireTime + reloadTime < Time.time)
+        {
+            for (int i = 0; i < numBullets; i++)
+            {
+                SpawnBullet();
+            }
+        }
+    }
+
+    private void SpawnBullet()
+    {
+        Bullet spawnedBullet = (Bullet) Instantiate(bullet);
+        Rigidbody2D body = spawnedBullet.GetComponent<Rigidbody2D>();
+        body.velocity = launchVec;
+        spawnedBullet.transform.Rotate(Random.Range(spread, spread * -1), 0, 0);
+    }
+
+}
