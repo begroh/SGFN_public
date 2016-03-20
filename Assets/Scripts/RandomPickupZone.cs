4e5c781c0121f3684b82191b1b0d519e610c1d69
emriedel <emriedel@umich.edu>
2016-03-20 15:04:23 -0400

    Random pickup zone prefab

---
+++ Assets/Scripts/RandomPickupZone.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomPickupZone : PickupZone {
+
+	public float checkRadius = 1;
+	public LayerMask walls = -1;
+
+	override protected void SpawnItem()
+	{
+		Collectible c = (Collectible) Instantiate(collectible);
+
+		float newX = 0, newY = 0;
+		Vector2 newPos = Vector3.zero;
+		Collider2D coll = null;
+
+		do {
+			float minX = this.transform.position.x - (this.transform.localScale.x / 2f);
+			float maxX = this.transform.position.x + (this.transform.localScale.x / 2f);
+
+			float minY = this.transform.position.y - (this.transform.localScale.y / 2f);
+			float maxY = this.transform.position.y + (this.transform.localScale.y / 2f);
+
+			newX = Random.Range (minX, maxX);
+			newY = Random.Range (minY, maxY);
+			newPos = new Vector2(newX, newY);
+
+			coll = Physics2D.OverlapCircle(newPos, checkRadius, walls);
+
+		} while(coll != null);
+			
+		c.transform.position = newPos;
+
+		c.pickup = this;
+	}
+}
