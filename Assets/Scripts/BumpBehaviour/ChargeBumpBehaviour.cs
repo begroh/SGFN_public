b1b1127ce7d716299a49cd94bbfd4c0041cd9e75
John DeSilva <desilvjo@umich.edu>
2016-03-22 14:10:18 -0400

    Add ChargeBumpBehaviour

---
+++ Assets/Scripts/BumpBehaviour/ChargeBumpBehaviour.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ChargeBumpBehaviour : BumpBehaviour
+{
+    private float charge = 0;
+    private float rate = 0.015f;
+
+    private float strength = 125f;
+
+    private float lastBump = 0;
+    private float bumpWait = 2f;
+
+    public void Update(Player player, bool bumping)
+    {
+        if (lastBump == 0 || Time.time - lastBump > bumpWait)
+        {
+            Rigidbody2D body = player.gameObject.GetComponent<Rigidbody2D>();
+
+            if (bumping)
+            {
+                if (charge < 1.0f)
+                {
+                    charge += rate;
+                }
+
+                body.velocity = body.velocity * (1 - charge);
+            }
+            else
+            {
+                if (charge > 0)
+                {
+                    body.AddForce(charge * strength * Vector2.up, ForceMode2D.Impulse);
+                    charge = 0;
+                }
+            }
+        }
+    }
+}
