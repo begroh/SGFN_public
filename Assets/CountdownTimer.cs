1ee3fecb58b6d6951f1887f8c2b2ccfcd2534b21
emriedel <emriedel@umich.edu>
2016-03-23 12:36:36 -0400

    Game timer prefab

---
+++ Assets/CountdownTimer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour {
+
+	public float timeLeft = 100;
+
+	Text text;
+
+	void Awake ()
+	{
+		text = this.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+
+		int minutesLeft = (int)timeLeft / 60;
+		int secondsLeft = (int)timeLeft % 60;
+
+
+		text.text = "" + minutesLeft + ":" + secondsLeft;
+		if(timeLeft <= 0) {
+			//Application.LoadLevel("gameOver");
+		}
+	}
+}
