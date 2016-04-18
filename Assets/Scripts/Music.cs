66d0f866ef268491802b5dea2acd729cc389924e
John DeSilva <desilvjo@umich.edu>
2016-04-18 18:59:39 -0400

    Add Music singleton

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private static Music instance;
+    private static AudioSource source;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            source = GetComponent<AudioSource>();
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        AudioClip clip = (AudioClip) Resources.Load("Audio/Sounds/Background", typeof(AudioClip));
+        instance.Play(clip);
+    }
+
+    public void Play(AudioClip clip)
+    {
+        if (source == null)
+        {
+            return;
+        }
+
+        source.Stop();
+        source.clip = clip;
+        source.Play();
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        source.PlayOneShot(clip);
+    }
+}
